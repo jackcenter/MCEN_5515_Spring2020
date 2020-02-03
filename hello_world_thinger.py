474ee8800923faea10900b011c9450c1604b674e
Chadd Miller <millerchadd21@gmail.com>
2020-02-03 11:28:12 -0700

    Leaving message in this space.  Hello World

---
+++ hello_world_thinger.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 11:22:28 2020
+
+@author: chadd
+"""
+
+
+A = ['char = string', 2, 3.14159265, "String"]
+
+x = 52
+
+print("I'm going to print!")
+print(x)
+print(A)
