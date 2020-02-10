5d230361f7cd7db930feb272394d6a36746c790c
Chadd Miller <millerchadd21@gmail.com>
2020-02-10 16:58:24 -0700

    widened boundaries

--- cyclone_game/videostream_method_online.py
+++ cyclone_game/videostream_method_online.py
@@ -38,8 +38,8 @@ while True:
 	# Show video stream
     cv2.imshow('orig', frame)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_green = np.array([59, 99, 49])
-    upper_green = np.array([121, 101, 101])
+    lower_green = np.array([59, 10, 10])
+    upper_green = np.array([121, 245, 245])
 
     mask = cv2.inRange(hsv, lower_green, upper_green)
     result = cv2.bitwise_and(frame, frame, mask=mask)   
