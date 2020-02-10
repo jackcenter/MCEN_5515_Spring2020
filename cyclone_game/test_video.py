3b6ccc1d885bf991c15411b248e6148fd1236221
Jack Center <jace3373@gmail.com>
2020-02-10 16:07:13 -0700

    doing jpegs now

--- cyclone_game/test_video.py
+++ cyclone_game/test_video.py
@@ -1,6 +1,7 @@
 # import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+import io
 import time
 import cv2
 
@@ -8,11 +9,11 @@ import cv2
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+rawCapture = io.Bytes()
 # allow the camera to warmup
 time.sleep(0.1)
 # capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+for frame in camera.capture_continuous(rawCapture, format="jpeg", use_video_port=True):
 	# grab the raw NumPy array representing the image, then initialize the timestamp
 	# and occupied/unoccupied text
 	image = frame.array
