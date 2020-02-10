3d79a6c61f1d10b41ca5d4fea807e2681390514a
Jack Center <jace3373@gmail.com>
2020-02-10 11:01:20 -0700

    added color filtering

--- cyclone_game/cwm_color_filtering_basics.py
+++ cyclone_game/cwm_color_filtering_basics.py
@@ -8,22 +8,14 @@ Created on Mon Feb 10 10:45:59 2020
 import cv2
 import numpy as np
 
-
-
 while True:
     # frame = Jack gets
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    lower_green = np.array([0,0,0])
-    upper_green = np.array([255,255,255])
+    lower_green = np.array([0, 0, 0])
+    upper_green = np.array([255, 255, 255])
     
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    res =  cv2.bitwise_and(frame, frame, mask = mask)
-    
-    
-    
-    
-cv2.destroyAllWindows()
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
 
-    
-    
+cv2.destroyAllWindows()
