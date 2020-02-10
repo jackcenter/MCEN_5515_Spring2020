ae7a178e7d6bcb185b2b3ebbdb766e7938b18fbc
Chadd Miller <millerchadd21@gmail.com>
2020-02-10 16:36:57 -0700

    second commit of the videostream method--changed indents to 4 spaces

--- cyclone_game/videostream_method_online.py
+++ cyclone_game/videostream_method_online.py
@@ -21,41 +21,40 @@ frameSize = (320, 240)
 
 # Initialize mutithreading the video stream.
 vs = VideoStream(src=0, usePiCamera=usingPiCamera, resolution=frameSize,
-		framerate=32).start()
+                 framerate=32).start()
 # Allow the camera to warm up.
 time.sleep(2.0)
 
 timeCheck = time.time()
 while True:
 	# Get the next frame.
-	frame = vs.read()
+    frame = vs.read()
 	
 	# If using a webcam instead of the Pi Camera,
 	# we take the extra step to change frame size.
-	if not usingPiCamera:
-		frame = imutils.resize(frame, width=frameSize[0])
+    if not usingPiCamera:
+        frame = imutils.resize(frame, width=frameSize[0])
 
 	# Show video stream
-	cv2.imshow('orig', frame)
+    cv2.imshow('orig', frame)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
     lower_green = np.array([0, 0, 0])
     upper_green = np.array([255, 255, 255])
-    
+
     mask = cv2.inRange(hsv, lower_green, upper_green)
     result = cv2.bitwise_and(frame, frame, mask=mask)   
     cv2.imshow('mask', mask)
     cv2.imshow('result', result)
-    
-    
-	key = cv2.waitKey(1) & 0xFF
+
+
+    key = cv2.waitKey(1) & 0xFF
 
 	# if the `q` key was pressed, break from the loop.
-	if key == ord("q"):
-		break
+    if key == ord("q"):
+        break
 	
-	print(1/(time.time() - timeCheck))
-	timeCheck = time.time()
+    print(1/(time.time() - timeCheck))
+    timeCheck = time.time()
 
 # Cleanup before exit.
 cv2.destroyAllWindows()
